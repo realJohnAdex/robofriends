58ab93fbaf0db930b80c03b522486698277d3c0b
realJohnAdex <101872587+realJohnAdex@users.noreply.github.com>
2022-05-13 13:02:10 +0100

    Updates

---
+++ precache-manifest.0a5d50a6d5c6f43a4a3aec4600798ba2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae8ecd888730c5329572",
+    "url": "/static/css/main.3ec6608b.chunk.css"
+  },
+  {
+    "revision": "ae8ecd888730c5329572",
+    "url": "/static/js/main.ae8ecd88.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "8f0e3b8c440636a5c25e",
+    "url": "/static/css/2.30051afe.chunk.css"
+  },
+  {
+    "revision": "8f0e3b8c440636a5c25e",
+    "url": "/static/js/2.8f0e3b8c.chunk.js"
+  },
+  {
+    "revision": "82555cf690c0363f7ef9",
+    "url": "/static/js/runtime~main.82555cf6.js"
+  },
+  {
+    "revision": "75b71c0475a43eabc4cf92633eb812c4",
+    "url": "/index.html"
+  }
+];
